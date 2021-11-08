7ae84e3c74749dc0c3a5bb9691151deb20ea9eb4
aliramadhan <yuukikimisaki@gmail.com>
2021-11-08 15:53:13 +0700

    Initial commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Image;
+
+class UploadController extends Controller
+{
+	public function index()
+    {
+        return view('Upload.upload');
+    }
+ 
+    public function imageFileUpload(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpg,jpeg,png,gif,svg|max:4096',
+        ]);
+
+        $fileName = time().'.'.$request->file('image')->getClientOriginalExtension();
+
+        $image = Image::make($request->file('image')->getRealPath());
+        $watermark = Image::make($request->file('watermark')->getRealPath())->resize(100,100);
+
+        /* insert watermark at bottom-right corner with 10px offset */
+        $image->insert($watermark, 'bottom-right', 10, 10);
+
+        $image->save(public_path($fileName));
+
+        return back()
+        	->with('success','File successfully uploaded.')
+        	->with('fileName',$fileName);         
+    }
+}
